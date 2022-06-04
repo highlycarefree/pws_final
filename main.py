e93006424ea01316818e14e1c50946b6b038e8e4
highlycarefree <highlycarefree@gmail.com>
2022-06-05 01:38:32 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # 刺探敵情小幫手
 ## Step 1. 事前下載
+'''
 1. 在終端機輸入以下指令以下載必要模組：
-```python= 
-pip install selenium
-pip install beautifulsoup4
-```
-2. 至 [https://sites.google.com/chromium.org/driver/](https://) 下載ChromeDriver，安裝完畢後複製路徑
-3. 下載資料夾中檔名為「2021_Facebook_icon.svg.png」的圖片，並與程式碼.py檔案放在同一資料夾中，作為後續文字雲底圖使用。
+pip install selenium BeautifulSoup requests jieba numpy scipy wordcloud
+2. 至以下網址下載ChromeDriver，安裝完畢後複製路徑
+https://sites.google.com/chromium.org/driver/
+'''
 
 ## Step 2. 安裝並匯入相關套件或模組
-```python= 
-#爬蟲相關套件
+# 爬蟲相關套件
 from bs4 import BeautifulSoup as Soup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,13 +19,13 @@ import math
 import os
 import requests
 import heapq
+from getpass import getpass
 
-#折線圖相關套件
+# 折線圖相關套件
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif'] = ['PingFang HK'] #處理matplotlib無法顯示中文的問題
 
-#文字雲相關套件
+# 文字雲相關套件
 import jieba
 from PIL import Image
 import numpy as np
@@ -37,11 +35,10 @@ import wordcloud
 # 時間相關套件
 from datetime import datetime
 import time
-```
+
 ## Step 3. 爬蟲
-```python= 
 while True:
-    #阻擋彈出式頁面
+    # 阻擋彈出式頁面
     options = webdriver.ChromeOptions()
     prefs = {
         'profile.default_content_setting_values' :
@@ -51,7 +48,7 @@ while True:
     }
     options.add_experimental_option('prefs', prefs)
     
-    #開啟 Facebook頁面
+    # 開啟Facebook頁面
     driver_path = input('請輸入webdriver路徑：')
     driver = webdriver.Chrome(driver_path, options = options)
     url = 'https://www.facebook.com'
@@ -59,7 +56,7 @@ while True:
     url = input('請貼上粉專網址：') 
     driver.get(url)
     
-    #將該社團或粉絲專頁的貼文載入
+    # 將該社團或粉絲專頁的貼文載入
     # 捲動頁面
     scrolls = int(input('請輸入希望捲動的頁面次數：'))
     time.sleep(3)
@@ -68,14 +65,14 @@ while True:
         time.sleep(3)
         
     root = Soup(driver.page_source, "html.parser")
-    #粉專名稱爬蟲
+    # 粉專名稱爬蟲
     Name_frame = root.find("div", class_= 'bi6gxh9e aov4n071')
     try:
         the_name = Name_frame.find("span", class_ = 'd2edcug0 hpfvmrgz qv66sw1b c1et5uql oi732d6d ik7dh3pa ht8s03o8 a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 ns63r2gh rwim8176 m6dqt4wy h7mekvxk hnhda86s oo9gr5id hzawbc8m').text
     except:
         the_name = Name_frame.find("h1", class_ = 'gmql0nx0 l94mrbxd p1ri9a11 lzcic4wl').text
 
-    #標題爬蟲
+    # 標題爬蟲
     titles_list = []
     temporary_post = []
     titles = root.find_all("div", class_= 'ecm0bbzt hv4rvrfc ihqw7lf3 dati1w0a')
@@ -88,7 +85,7 @@ while True:
                 temporary_post.append(post.text)
             titles_list.append(temporary_post)
 
-    # 貼文的link 爬蟲
+    # 貼文的link爬蟲
     frames = root.find_all(class_ = 'll8tlv6m j83agx80 btwxx1t3 n851cfcs hv4rvrfc dati1w0a pybr56ya')
     links = []
     for ii in frames: 
@@ -162,24 +159,25 @@ while True:
     for k in range(len(shares)):
         k = int(k)
         
-    #確定爬蟲資料無誤，若有誤會重新再爬一次
+    # 確定爬蟲資料無誤，若有誤會重新再爬一次
     if len(times) == len(likes) == len(comments) == len(shares) and len(likes) > 3 :
         break
     else:
         print('很抱歉！受網路速度影響，導致爬蟲結果不完整，請重新輸入。')
         continue
            
-#找出讚數前三高的貼文連結進行留言爬蟲
+# 找出讚數前三高的貼文連結進行留言爬蟲
 best_3 = map(likes.index, heapq.nlargest(3,likes))
 best_links = []
 for z in best_3:
     z = int(z)
     best_links.append(links[z])
         
-#留言爬蟲
+# 留言爬蟲
 comments_list = []
 account = input('請輸入帳號：')
-psw = input('請輸入密碼：')
+# psw = input('請輸入密碼：')
+psw = getpass('請輸入密碼：')
 for i in range(3):  
     driver.get(best_links[i])
     wait = WebDriverWait(driver, 30)
@@ -202,17 +200,16 @@ for i in range(3):
                 comments_list.append(cont.text)
         else:
             comments_list.append('')
-```
 
 ## Step 4. 視覺化｜繪製折線圖
-```python=
-# 設定圖片解析度與長寬、背景色
+# 設定圖片的中文字型、解析度、長寬與背景色
+plt.rcParams['font.sans-serif'] = ['PingFang HK'] # 處理matplotlib無法顯示中文的問題
 fig = plt.figure(figsize = (20,10), dpi = 500, linewidth = 2) 
 font1 = {'weight': 'normal', 'size': 18} 
 plt.rcParams['axes.facecolor'] = 'none'
 plt.rcParams['savefig.facecolor'] = 'none'
 
-# 將數據從list改為numpy array格式，以避免ValueError
+# 將數據從list改為ndarray格式，以避免ValueError
 times = np.array(times)
 likes = np.array(likes)
 comments = np.array(comments)
@@ -241,9 +238,8 @@ for x, y in zip(times, comments):
     plt.text(x, y, y, ha = 'center', va = 'bottom', fontsize = 15)
 for x, y in zip(times, shares):
     plt.text(x, y, y, ha = 'center', va = 'top', fontsize = 15)
-```
+
 ## Step 5. 視覺化｜繪製文字雲
-```python=
 # reading the article
 article = ""
 for tmp in comments_list:
@@ -279,9 +275,8 @@ c = wordcloud.WordCloud(font_path = '/System/Library/Fonts/PingFang.ttc', \
                         stopwords = sw, \
                         min_word_length = 1)
 c.generate(j)
-```
+
 ## Step 6. 處理輸出
-```python=
 # output with time stamp
 local_time = time.localtime()
 output_time = time.strftime('%m%d%H%M', local_time)
